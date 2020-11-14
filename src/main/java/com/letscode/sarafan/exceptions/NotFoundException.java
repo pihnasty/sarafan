d6d0123564bc7c9832c9467531125727342a65ab
POM <pom7@bk.ru>
2020-11-14 14:23:27 +0200

    Initial commit

---
+++ src/main/java/com/letscode/sarafan/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.letscode.sarafan.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
